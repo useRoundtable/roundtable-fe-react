78a2214ac350bf90c9d8834a3d46bd5a1ee35ee1
Tony <tonyrkovar@gmail.com>
2020-06-02 14:28:21 -0500

    chore: put queries and mutations into seperate files

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -2,24 +2,14 @@ import React from "react";
 import { NavBar } from "../navigation/NavBar";
 import { MyRoundtableList } from "../myRoundtableList/MyRoundtableList";
 import { useQuery } from "@apollo/react-hooks";
-import { gql } from "apollo-boost";
 
-const USERS = gql`
-	{
-		users {
-			id
-			email
-			firstName
-			lastName
-		}
-	}
-`;
+import { USERS as userData } from "../../resolvers/queries";
 
 export const Dashboard = () => {
-	// const { loading, error, data } = useQuery(USERS);
-
-	// if (loading) return <h1>LOADING....</h1>;
-	// if (error) return { error };
+	const { loading, error, data } = useQuery(userData);
+	
+	if (loading) return <h1>LOADING....</h1>;
+	if (error) return console.log(error), null;
 	return (
 		<>
 			<NavBar />
