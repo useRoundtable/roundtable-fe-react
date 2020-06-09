fea105a3263c7bca2d6dc14d7bc060b4a8953096
Steven Kovar <steven.kovar@gmail.com>
2020-06-09 17:32:59 -0500

    Changes for 6-9-20

--- src/components/roundtable/roundtable.jsx
+++ src/components/roundtable/roundtable.jsx
@@ -73,18 +73,61 @@ export const Roundtable = () => {
 					</ul>
 				</nav>
 				<div className="content">
-					<section className="issueList">
+
+					<section className="issue view">
+						<article className="issueHeader">
+							<h3 className="issue">
+								<span className="number">Issue #4</span>
+								<span className="title">Hey everyone!</span>
+							</h3>
+						</article>
+						<article className="message">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tempor dolor nec lorem pharetra, quis cursus purus iaculis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisl nisl, blandit eget arcu sit amet, euismod molestie mi. Vivamus molestie, sem nec lobortis malesuada, risus enim gravida nisi, non volutpat dui est nec ante. Fusce a felis at libero mattis scelerisque ac at neque. Aenean metus augue, tristique iaculis diam eu, commodo facilisis lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque vehicula massa a massa tempus, sit amet vestibulum nisl vestibulum.</p>
+							<p>Phasellus quis suscipit augue, et efficitur sem. Vestibulum pharetra justo condimentum tincidunt condimentum. Donec quis libero nec erat sollicitudin elementum. Etiam non volutpat ligula, ut faucibus quam. Suspendisse consequat urna eu iaculis euismod. In eu viverra diam. Nunc ac ligula eget nibh volutpat hendrerit. Sed eget mollis felis. Duis ullamcorper sodales viverra. Suspendisse erat lorem, consectetur sit amet risus a, eleifend gravida sapien. Sed hendrerit mollis erat, ac bibendum massa commodo eleifend. Nulla purus lectus, varius id mauris nec, scelerisque bibendum ipsum. Maecenas ac mattis massa, quis eleifend ipsum.</p>
+							<p>Donec suscipit turpis interdum, lobortis magna a, suscipit odio. Duis sit amet dignissim sapien. Cras tincidunt, odio id venenatis ultricies, mauris nunc pulvinar velit, eu condimentum mi purus nec felis. Suspendisse eu purus vitae tortor pellentesque hendrerit. Sed egestas finibus mattis. Donec ultrices sapien nec augue volutpat tincidunt. Curabitur eu nulla quam. Duis fringilla venenatis viverra. Proin mollis erat tellus, maximus condimentum purus consectetur eget. Donec dictum risus ut sapien semper egestas. Morbi ac nunc euismod, blandit turpis vitae, dignissim enim. Aliquam ut libero maximus, luctus justo et, ultricies ex. Donec vel nisl nisi.</p>
+							<ul className="stats"><li className="members"><ul className="members"><li><img src="https://pbs.twimg.com/profile_images/1217113645907349505/u6wRx3nJ_400x400.jpg" alt="Steven K." /></li></ul>Steven K.</li><li className="date">June 6, 2020</li><li className="responses new">32 New Responses</li></ul>
+						</article>
+					</section>
+
+					<section className="issueCreate">
 						<h4>
-							4 Issues <em>.issueList</em>
+							<em>.issueCreate</em>
 						</h4>
+						<h2>Title</h2>
+						<ul>
+							<h3>Questions</h3>
+							<li>
+								<h4>Question 1</h4>
+							</li>
+						</ul>
+					</section>
+
+					<section className="issueRespond">
+						<h4>
+							<em>.issueRespond</em>
+						</h4>
+						<h2>Title</h2>
+						<ul>
+							<h3>Questions</h3>
+							<li>
+								<h4>Question 1</h4>
+							</li>
+						</ul>
+					</section>
+
+					<section className="issueList">
 						<ul className="issueList">
 							<li className="new">
 								<h3 className="issue">
-									<span className="number">Start Issue #5</span>
+									<span className="number">Issue #5</span>
 									<span className="title">New Issue&hellip;</span>
 									<a className="button">+ Create</a>
 								</h3>
 							</li>
+
+						</ul>
+						<h4>Current Issue</h4>
+						<ul className="issueList currentIssue">
 							<li>
 								<h3 className="issue">
 									<span className="number">Issue #4</span>
@@ -93,7 +136,7 @@ export const Roundtable = () => {
 									<span className="message">Zefhir’s is a parachute rescue system for turbine helicopters. Installed above the main rotor, the parachute is designed as a backup for conditions where autorotation cannot be performed. Zefhir’s is a parachute rescue system for turbine helicopters. Installed above the main rotor, the parachute is designed as a backup for conditions where autorotation cannot be performed. Zefhir’s is a parachute rescue system for turbine helicopters. Installed above the main rotor, the parachute is designed as a backup for conditions where autorotation cannot be performed.Zefhir’s is a parachute rescue system for turbine helicopters. Installed above the main rotor, the parachute is designed as a backup for conditions where autorotation cannot be performed.</span>
 								</h3>
 								<ul className="options">
-									<li class="priority">
+									<li className="priority">
 										<a className="button new" onClick={(e) => enterRoundtable(e, "/roundtable")}>
 											12 New Responses
 										</a>
@@ -116,6 +159,9 @@ export const Roundtable = () => {
 									<li className="more">Edit Issue</li>
 								</ul>
 							</li>
+						</ul>
+						<h4>4 Previous Issues <em>.issueList</em></h4>
+						<ul className="issueList allIssues">
 							<li>
 								<h3 className="issue">
 									<span className="number">Issue #3</span>
@@ -124,7 +170,7 @@ export const Roundtable = () => {
 									<span className="message">Zefhir’s is a parachute rescue system for turbine helicopters. Installed above the main rotor, the parachute is designed as a backup for conditions where autorotation cannot be performed. Zefhir’s is a parachute rescue system for turbine helicopters. Installed above the main rotor, the parachute is designed as a backup for conditions where autorotation cannot be performed. Zefhir’s is a parachute rescue system for turbine helicopters. Installed above the main rotor, the parachute is designed as a backup for conditions where autorotation cannot be performed.Zefhir’s is a parachute rescue system for turbine helicopters. Installed above the main rotor, the parachute is designed as a backup for conditions where autorotation cannot be performed.</span>
 								</h3>
 								<ul className="options">
-									<li class="priority">
+									<li className="priority">
 										<a className="button new" onClick={(e) => enterRoundtable(e, "/roundtable")}>
 											2 New Responses
 										</a>
@@ -207,12 +253,12 @@ export const Roundtable = () => {
 									<span className="message">Zefhir’s is a parachute rescue system for turbine helicopters. Installed above the main rotor, the parachute is designed as a backup for conditions where autorotation cannot be performed. Zefhir’s is a parachute rescue system for turbine helicopters. Installed above the main rotor, the parachute is designed as a backup for conditions where autorotation cannot be performed. Zefhir’s is a parachute rescue system for turbine helicopters. Installed above the main rotor, the parachute is designed as a backup for conditions where autorotation cannot be performed.Zefhir’s is a parachute rescue system for turbine helicopters. Installed above the main rotor, the parachute is designed as a backup for conditions where autorotation cannot be performed.</span>
 								</h3>
 								<ul className="options">
-									<li class="priority">
+									<li className="priority">
 										<a className="button" onClick={(e) => enterRoundtable(e, "/roundtable")}>
 											View Issue
 										</a>
 									</li>
-									<li class="priority">
+									<li className="priority">
 										<a className="button notPriority" onClick={(e) => enterRoundtable(e, "/roundtable")}>
 											Public Link
 										</a>
@@ -323,43 +369,7 @@ export const Roundtable = () => {
 							</li>
 						</ul>
 					</section>
-					<section className="issueCreate">
-						<h4>
-							<em>.issueCreate</em>
-						</h4>
-						<h2>Title</h2>
-						<ul>
-							<h3>Questions</h3>
-							<li>
-								<h4>Question 1</h4>
-							</li>
-						</ul>
-					</section>
-					<section className="issueRespond">
-						<h4>
-							<em>.issueRespond</em>
-						</h4>
-						<h2>Title</h2>
-						<ul>
-							<h3>Questions</h3>
-							<li>
-								<h4>Question 1</h4>
-							</li>
-						</ul>
-					</section>
-					<section className="issueView">
-						<h4>
-							<em>.issueView</em>
-						</h4>
-						<h2>Title</h2>
-						<ul>
-							<h3>Questions</h3>
-							<li>
-								<h4>Question 1</h4>
-							</li>
-						</ul>
-					</section>
-					<h2>Ready</h2>
+
 					<section className="members">
 						<h4>
 							10 Members <em>.memberList</em>
@@ -476,54 +486,6 @@ export const Roundtable = () => {
 								</span>
 								<h4>Steven Michael Kovar Steven Michael Kovar</h4>
 								<a href="javascript:void(0)" className="button">Invited</a>
-							</li>
-						</ul>
-					</section>
-					<section className="issueList">
-						<h4>
-							6 Issues <em>.issueList</em>
-						</h4>
-						<h2>Title</h2>
-						<ul>
-							<h3>Questions</h3>
-							<li>
-								<h4>Question 1</h4>
-							</li>
-						</ul>
-					</section>
-					<section className="issueCreate">
-						<h4>
-							<em>.issueCreate</em>
-						</h4>
-						<h2>Title</h2>
-						<ul>
-							<h3>Questions</h3>
-							<li>
-								<h4>Question 1</h4>
-							</li>
-						</ul>
-					</section>
-					<section className="issueRespond">
-						<h4>
-							<em>.issueRespond</em>
-						</h4>
-						<h2>Title</h2>
-						<ul>
-							<h3>Questions</h3>
-							<li>
-								<h4>Question 1</h4>
-							</li>
-						</ul>
-					</section>
-					<section className="issueView">
-						<h4>
-							<em>.issueView</em>
-						</h4>
-						<h2>Title</h2>
-						<ul>
-							<h3>Questions</h3>
-							<li>
-								<h4>Question 1</h4>
 							</li>
 						</ul>
 					</section>
