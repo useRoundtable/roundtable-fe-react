9c8370598d1f7a7669b29d82522381e2d996b5df
Steven Kovar <steven.kovar@gmail.com>
2020-06-16 15:58:56 -0500

    wat

--- src/components/roundtable/roundtable.jsx
+++ src/components/roundtable/roundtable.jsx
@@ -230,12 +230,13 @@ export const Roundtable = () => {
 									</ul>
 									Tony K.
 								</li>
+								<li className="textCount noHover noRemaining"><em>255</em> / 255</li>
 							</ul>
 							<textarea type="text" placeholder="What was your major accomplishment this week?"></textarea>
 						</article>
 						<article className="question">
 							<div className="questionContent">
-								<h6 className="questionNumber">Question 2 (.answer)</h6>	
+								<h6 className="questionNumber">Question 2</h6>	
 								<h4 className="question">What do you need help with for next week?</h4>
 								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tempor dolor nec lorem pharetra, quis cursus purus iaculis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 								<p>Fusce a felis at libero mattis scelerisque ac at neque. Aenean metus augue, tristique iaculis diam eu, commodo facilisis lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque vehicula massa a massa tempus, sit amet vestibulum nisl vestibulum.</p>
@@ -252,6 +253,7 @@ export const Roundtable = () => {
 									</ul>
 									Tony K.
 								</li>
+								<li className="textCount noHover"><em>25</em> / 255</li>
 							</ul>
 							<textarea type="text" placeholder="What do you need help with for next week?"></textarea>
 						</article>
@@ -272,6 +274,7 @@ export const Roundtable = () => {
 									</ul>
 									Tony K.
 								</li>
+								<li className="textCount noHover"><em>0</em> / 255</li>
 							</ul>
 							<textarea type="text" placeholder="What does it look like with no paragraphs?"></textarea>
 						</article>
