bd27f8c303217fea31d4fe906b020ba4c1b3ad00
Tony <tonyrkovar@gmail.com>
2020-06-23 13:08:38 -0500

    feat/chore: autosave formatting && hooked up the createUser mutation to register form

--- src/components/home/homeContent/join/Join.jsx
+++ src/components/home/homeContent/join/Join.jsx
@@ -1,42 +1,126 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { useMutation } from "@apollo/react-hooks";
+import { RTAnimation } from "../../../animation/roundtableAni";
+
+import { REGISTER as register } from "../../../../resolvers/mutations";
 
 export const Join = () => {
-	const [email, setEmail] = useState("")
-	const [username, setUsername] = useState("")
-	const [firstName, setFirstName] = useState("")
-	const [lastName, setLastName] = useState("")
-	const [password, setPassword] = useState("")
-	const [confirmPassword, setConfirmPassword] = useState("")
+	const [email, setEmail] = useState("");
+	const [username, setUsername] = useState("");
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+	const [password, setPassword] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
+
+	const [registerUser, { loading }] = useMutation(register, {
+		onCompleted({ createUser }) {
+			console.log(createUser.token);
+			localStorage.setItem("authorization", `Bearer ${createUser.token}`);
+			window.location.assign("/dashboard");
+		},
+	});
+	// if (loading) {
+	// 	return (
+	// 		<article className="join">
+	// 			<RTAnimation members={[]} />
+	// 		</article>
+	// 	);
+	// }
+
 	return (
 		<article className="join">
 			<h3>Your first Roundtable</h3>
 			<form>
-			<div className="field">
-				<input name="join-email" id="join-email" type="email" placeholder="" value={email} onChange={e => setEmail(e.target.value)} required />
-				<label for="join-email">Email</label>
-			</div>
-			<div className="field">
-				<input name="join-username" id="join-username" type="text" placeholder="" value={username} onChange={e => setUsername(e.target.value)} required />
-				<label for="join-username">Username</label>
-			</div>
-			<div className="field">
-				<input name="join-firstName" id="join-firstName" type="text" placeholder="" value={firstName} onChange={e => setFirstName(e.target.value)} required />
-				<label for="join-firstName">First Name</label>
-			</div>
-			<div className="field">
-				<input name="join-lastName" id="join-lastName" type="text" placeholder="" value={lastName} onChange={e => setLastName(e.target.value)} required />
-				<label for="join-lastName">Last Name</label>
-			</div>
-			<div className="field">
-				<input name="join-password" id="join-password" type="password" placeholder="" value={password} onChange={e => setPassword(e.target.value)} required />
-				<label for="join-password">Password</label>
-			</div>
-			<div className="field">
-				<input name="join-confirmPassword" id="join-confirmPassword" type="password" placeholder="" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} required />
-				<label for="join-confirmPassword">Confirm Password</label>
-			</div>
-			<a className="button big" href="javascript:void(0)">Get Started</a>
+				<div className="field">
+					<input
+						name="join-email"
+						id="join-email"
+						type="email"
+						placeholder=""
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						required
+					/>
+					<label for="join-email">Email</label>
+				</div>
+				<div className="field">
+					<input
+						name="join-username"
+						id="join-username"
+						type="text"
+						placeholder=""
+						value={username}
+						onChange={(e) => setUsername(e.target.value)}
+						required
+					/>
+					<label for="join-username">Username</label>
+				</div>
+				<div className="field">
+					<input
+						name="join-firstName"
+						id="join-firstName"
+						type="text"
+						placeholder=""
+						value={firstName}
+						onChange={(e) => setFirstName(e.target.value)}
+						required
+					/>
+					<label for="join-firstName">First Name</label>
+				</div>
+				<div className="field">
+					<input
+						name="join-lastName"
+						id="join-lastName"
+						type="text"
+						placeholder=""
+						value={lastName}
+						onChange={(e) => setLastName(e.target.value)}
+						required
+					/>
+					<label for="join-lastName">Last Name</label>
+				</div>
+				<div className="field">
+					<input
+						name="join-password"
+						id="join-password"
+						type="password"
+						placeholder=""
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						required
+					/>
+					<label for="join-password">Password</label>
+				</div>
+				<div className="field">
+					<input
+						name="join-confirmPassword"
+						id="join-confirmPassword"
+						type="password"
+						placeholder=""
+						value={confirmPassword}
+						onChange={(e) => setConfirmPassword(e.target.value)}
+						required
+					/>
+					<label for="join-confirmPassword">Confirm Password</label>
+				</div>
+				<a
+					className="button big"
+					href="javascript:void(0)"
+					onClick={() => {
+						registerUser({
+							variables: {
+								email,
+								password,
+								firstName,
+								lastName,
+								userName: username,
+							},
+						});
+					}}
+				>
+					Get Started
+				</a>
 			</form>
 		</article>
-		);
+	);
 };
