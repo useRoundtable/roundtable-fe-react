f761e03f832c2056f6218ed07a7242d084341b58
Tony <tonyrkovar@gmail.com>
2020-07-09 11:39:30 -0500

    feat: new component for the RT member view

---
+++ src/components/roundtable/members/MemberCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export const MemberCard = ({ user }) => {
+	console.log(user);
+	return (
+		<li>
+			<span className="avatar">
+				<img
+					alt="avatar"
+					src="https://pbs.twimg.com/profile_images/1217113645907349505/u6wRx3nJ_400x400.jpg"
+				/>
+			</span>
+			<h4>{user.firstName ? user.firstName : user.userName}</h4>
+			<a href="javascript:void(0)" className="button">
+				View Profile
+			</a>
+			<div className="stats">
+				<span>2,572</span>
+				<span className="active">Active</span>
+			</div>
+		</li>
+	);
+};
