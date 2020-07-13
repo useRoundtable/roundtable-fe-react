e08fcc0da7b2924d472c20f75f4815de7a5f446e
Tony <tonyrkovar@gmail.com>
2020-07-13 14:56:13 -0500

    chore: Stopped the login component from re-rendering when redirecting to the dashboard on login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -8,16 +8,13 @@ import { LOGIN as login } from "../../resolvers/mutations";
 export const Login = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
-	const [userLogin, { loading }] = useMutation(login, {
+	const [userLogin] = useMutation(login, {
 		onCompleted({ login }) {
 			localStorage.setItem("authorization", `Bearer ${login.token}`);
 			window.location.assign("/dashboard");
 		},
 	});
 
-	if (loading) {
-		return <h1>Loading....</h1>;
-	}
 	return (
 		<>
 			<nav className="login">
@@ -34,44 +31,43 @@ export const Login = () => {
 					</li>
 				</ul>
 				<div class="get-started">
-				<form>
-					<div className="field">
-						<input
-							name="login-email"
-							id="login-email"
-							type="email"
-							placeholder=""
-							value={email}
-							onChange={(e) => setEmail(e.target.value)}
-							required
-						/>
-						<label for="login-email">Email</label>
-					</div>
-					<div className="field">
-						<input
-							name="login-password"
-							id="login-password"
-							type="password"
-							placeholder=""
-							onChange={(e) => setPassword(e.target.value)}
-							value={password}
-							required
-						/>
-						<label for="login-password">Password</label>
-					</div>
-					<a
-						className="button"
-						// href="/dashboard"
-						onClick={() => {
-							userLogin({
-								variables: { email: email, password: password },
-							});
-						}}
-					>
-						Log In
-					</a>
-				</form>
-			</div>
+					<form>
+						<div className="field">
+							<input
+								name="login-email"
+								id="login-email"
+								type="email"
+								placeholder=""
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
+								required
+							/>
+							<label for="login-email">Email</label>
+						</div>
+						<div className="field">
+							<input
+								name="login-password"
+								id="login-password"
+								type="password"
+								placeholder=""
+								onChange={(e) => setPassword(e.target.value)}
+								value={password}
+								required
+							/>
+							<label for="login-password">Password</label>
+						</div>
+						<a
+							className="button"
+							onClick={() => {
+								userLogin({
+									variables: { email: email, password: password },
+								});
+							}}
+						>
+							Log In
+						</a>
+					</form>
+				</div>
 			</nav>
 		</>
 	);
