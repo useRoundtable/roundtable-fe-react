c0a66970670770f0dee41d46064fd24289a536a5
Tony <tonyrkovar@gmail.com>
2020-07-14 17:18:02 -0500

    Added WYSIWYG and broke down the issue creation component

--- src/components/roundtable/createIssue/CreateIssue.jsx
+++ src/components/roundtable/createIssue/CreateIssue.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { CreateIssueQuestion } from "./createIssueQuestion/CreateIssueQuestion";
+
 export const CreateIssue = () => {
 	return (
 		<section className="issue new">
@@ -10,7 +12,7 @@ export const CreateIssue = () => {
 					<span class="title">The music minute?!</span>
 				</h3>
 			</article>
-			<article className="message">
+			{/* <article className="message">
 				<ul className="stats">
 					<li className="members">
 						<ul className="members">
@@ -21,46 +23,168 @@ export const CreateIssue = () => {
 								/>
 							</li>
 						</ul>
-						{/* {data.issueById.issueAuthor.userName} says{" "} */} Steven K.
+						Steven K.
 					</li>
 				</ul>
-				<p>Growing up, I always assumed breakdowns in communication were strictly teenage issues—that adults somehow divined the answers in their transition from kid to grownup. Turns out I was wrong.</p>
-				<p>At home, in the office, or between friends, most issues in a meaningful relationship result from miscommunication happening at some point. Our initial reaction is to become aggressive or defensive, trying to "solve the problem" by winning the argument (wrong) or conceding to make the other person happy (wrong) rather than deconstructing the problem to find where communication broke down (right).</p>
+				<p>
+					Growing up, I always assumed breakdowns in communication were
+					strictly teenage issues—that adults somehow divined the answers
+					in their transition from kid to grownup. Turns out I was wrong.
+				</p>
+				<p>
+					At home, in the office, or between friends, most issues in a
+					meaningful relationship result from miscommunication happening at
+					some point. Our initial reaction is to become aggressive or
+					defensive, trying to "solve the problem" by winning the argument
+					(wrong) or conceding to make the other person happy (wrong)
+					rather than deconstructing the problem to find where
+					communication broke down (right).
+				</p>
 				<ol>
 					<li>Item 1</li>
 					<li>Item 2</li>
 					<li>Item 3</li>
 				</ol>
-				<p>Whether that emotional reaction stems from misaligned expectations, a lack of context, or <a href="http://www.nytimes.com/1991/09/17/science/non-verbal-cues-are-easy-to-misinterpret.html?pagewanted=all" title="NYT - Non-Verbal Cues Are Easy To Misinterpret">nonverbal miscues</a>, it's vital to avoid letting the situation escalate into an argument—which tend to be one-sided. I like what Ben Horowitz says about <a href="http://bhorowitz.com/2012/10/17/making-yourself-a-ceo/" title="Ben Horowitz - Making Yourself A CEO">feedback being a dialogue</a> vs. a monologue:</p>
-				<blockquote>You may be telling somebody about something that you don’t like or disagree with, but that doesn’t mean that you’re right... Your goal should be for your feedback to open up rather than close down discussion.</blockquote>
-				<p>The satisfaction of winning an argument doesn't benefit the relationship. It creates tension and is childish. If you are too riled up to have a dialogue, disconnect yourself from the moment and come back to discuss the cause of miscommunication when you are relaxed—it works wonders. I promise.</p>
-				<p>You want a mutual understanding of the <em>situation</em> and of each others' <em>feelings</em> so you can make an informed, collaborative decision. If you are having trouble articulating your feelings, try using this phrasing in your head or to your partner:</p>
+				<p>
+					Whether that emotional reaction stems from misaligned
+					expectations, a lack of context, or{" "}
+					<a
+						href="http://www.nytimes.com/1991/09/17/science/non-verbal-cues-are-easy-to-misinterpret.html?pagewanted=all"
+						title="NYT - Non-Verbal Cues Are Easy To Misinterpret"
+					>
+						nonverbal miscues
+					</a>
+					, it's vital to avoid letting the situation escalate into an
+					argument—which tend to be one-sided. I like what Ben Horowitz
+					says about{" "}
+					<a
+						href="http://bhorowitz.com/2012/10/17/making-yourself-a-ceo/"
+						title="Ben Horowitz - Making Yourself A CEO"
+					>
+						feedback being a dialogue
+					</a>{" "}
+					vs. a monologue:
+				</p>
 				<blockquote>
-				<p>When you say X in situation Y, it makes me feel Z.</p>
+					You may be telling somebody about something that you don’t like
+					or disagree with, but that doesn’t mean that you’re right... Your
+					goal should be for your feedback to open up rather than close
+					down discussion.
 				</blockquote>
-				<p>Similarly, Jeff Atwood mentions in his post <a href="http://www.codinghorror.com/blog/2012/06/how-to-talk-to-human-beings.html" title="Coding Horror - How To Talk To Human Beings">How to Talk to Human Beings</a> how a <a href="http://www.amazon.com/dp/0380811960" title="How to Talk So Kids Will Listen &amp; Listen So Kids Will Talk">parenting book</a> fundamentally changed how he deals with "children aged 2 to 99." The book provides a template for resolving tension: listen, repeat what they say, and label their emotions. The 'child' will find the solution themselves. Jeff's post shows an example of a discussion with an upset adult:</p>
+				<p>
+					The satisfaction of winning an argument doesn't benefit the
+					relationship. It creates tension and is childish. If you are too
+					riled up to have a dialogue, disconnect yourself from the moment
+					and come back to discuss the cause of miscommunication when you
+					are relaxed—it works wonders. I promise.
+				</p>
+				<p>
+					You want a mutual understanding of the <em>situation</em> and of
+					each others' <em>feelings</em> so you can make an informed,
+					collaborative decision. If you are having trouble articulating
+					your feelings, try using this phrasing in your head or to your
+					partner:
+				</p>
+				<blockquote>
+					<p>When you say X in situation Y, it makes me feel Z.</p>
+				</blockquote>
+				<p>
+					Similarly, Jeff Atwood mentions in his post{" "}
+					<a
+						href="http://www.codinghorror.com/blog/2012/06/how-to-talk-to-human-beings.html"
+						title="Coding Horror - How To Talk To Human Beings"
+					>
+						How to Talk to Human Beings
+					</a>{" "}
+					how a{" "}
+					<a
+						href="http://www.amazon.com/dp/0380811960"
+						title="How to Talk So Kids Will Listen &amp; Listen So Kids Will Talk"
+					>
+						parenting book
+					</a>{" "}
+					fundamentally changed how he deals with "children aged 2 to 99."
+					The book provides a template for resolving tension: listen,
+					repeat what they say, and label their emotions. The 'child' will
+					find the solution themselves. Jeff's post shows an example of a
+					discussion with an upset adult:
+				</p>
 				<ul>
 					<li>Item 1</li>
 					<li>Item 2</li>
 					<li>Item 3</li>
 				</ul>
-				<p>Similarly, Jeff Atwood mentions in his post <a href="http://www.codinghorror.com/blog/2012/06/how-to-talk-to-human-beings.html" title="Coding Horror - How To Talk To Human Beings">How to Talk to Human Beings</a> how a <a href="http://www.amazon.com/dp/0380811960" title="How to Talk So Kids Will Listen &amp; Listen So Kids Will Talk">parenting book</a> fundamentally changed how he deals with "children aged 2 to 99." The book provides a template for resolving tension: listen, repeat what they say, and label their emotions. The 'child' will find the solution themselves. Jeff's post shows an example of a discussion with an upset adult:</p>
-				<p>Similarly, Jeff Atwood mentions in his post <a href="http://www.codinghorror.com/blog/2012/06/how-to-talk-to-human-beings.html" title="Coding Horror - How To Talk To Human Beings">How to Talk to Human Beings</a> how a <a href="http://www.amazon.com/dp/0380811960" title="How to Talk So Kids Will Listen &amp; Listen So Kids Will Talk">parenting book</a> fundamentally changed how he deals with "children aged 2 to 99." The book provides a template for resolving tension: listen, repeat what they say, and label their emotions. The 'child' will find the solution themselves. Jeff's post shows an example of a discussion with an upset adult:</p>
+				<p>
+					Similarly, Jeff Atwood mentions in his post{" "}
+					<a
+						href="http://www.codinghorror.com/blog/2012/06/how-to-talk-to-human-beings.html"
+						title="Coding Horror - How To Talk To Human Beings"
+					>
+						How to Talk to Human Beings
+					</a>{" "}
+					how a{" "}
+					<a
+						href="http://www.amazon.com/dp/0380811960"
+						title="How to Talk So Kids Will Listen &amp; Listen So Kids Will Talk"
+					>
+						parenting book
+					</a>{" "}
+					fundamentally changed how he deals with "children aged 2 to 99."
+					The book provides a template for resolving tension: listen,
+					repeat what they say, and label their emotions. The 'child' will
+					find the solution themselves. Jeff's post shows an example of a
+					discussion with an upset adult:
+				</p>
+				<p>
+					Similarly, Jeff Atwood mentions in his post{" "}
+					<a
+						href="http://www.codinghorror.com/blog/2012/06/how-to-talk-to-human-beings.html"
+						title="Coding Horror - How To Talk To Human Beings"
+					>
+						How to Talk to Human Beings
+					</a>{" "}
+					how a{" "}
+					<a
+						href="http://www.amazon.com/dp/0380811960"
+						title="How to Talk So Kids Will Listen &amp; Listen So Kids Will Talk"
+					>
+						parenting book
+					</a>{" "}
+					fundamentally changed how he deals with "children aged 2 to 99."
+					The book provides a template for resolving tension: listen,
+					repeat what they say, and label their emotions. The 'child' will
+					find the solution themselves. Jeff's post shows an example of a
+					discussion with an upset adult:
+				</p>
 				<blockquote>
-				<p>“Oh come on! I can’t get a signal here? Dammit. This is New York.”</p>
-				<p>He looked at me.</p>
-				<p>“No signal?” I say. “Here in New York?” (Repeat what they say.)</p>
-				<p>“It’s not like we’re in Goddamn Wisconsin.”</p>
-				<p>“Mmmm.” (Listen. Make soothing noises.)</p>
-				<p>“We’re not on a farm. It’s New York, for God’s sake,” he said.</p>
-				<p>“That’s frustrating,” I say. (Label their emotions.)</p>
-				<p>He calmed down.</p>
+					<p>
+						“Oh come on! I can’t get a signal here? Dammit. This is New
+						York.”
+					</p>
+					<p>He looked at me.</p>
+					<p>
+						“No signal?” I say. “Here in New York?” (Repeat what they
+						say.)
+					</p>
+					<p>“It’s not like we’re in Goddamn Wisconsin.”</p>
+					<p>“Mmmm.” (Listen. Make soothing noises.)</p>
+					<p>
+						“We’re not on a farm. It’s New York, for God’s sake,” he said.
+					</p>
+					<p>“That’s frustrating,” I say. (Label their emotions.)</p>
+					<p>He calmed down.</p>
 				</blockquote>
-				<p>Next time you feel tension in a relationship, take a step back to gain some perspective and apply these concepts. Remember, it's not just kids who need to learn how to communicate.</p>
-			</article>
+				<p>
+					Next time you feel tension in a relationship, take a step back to
+					gain some perspective and apply these concepts. Remember, it's
+					not just kids who need to learn how to communicate.
+				</p>
+			</article> */}
 			<article className="questionList">
 				<ul className="questionList">
-					<h6 className="questionListHeader">Collect responses for&hellip;until July 17, 2020.</h6>
+					<h6 className="questionListHeader">
+						Collect responses for&hellip;until July 17, 2020.
+					</h6>
 					<li>1 day</li>
 					<li>2 days</li>
 					<li>3 days</li>
@@ -70,48 +194,7 @@ export const CreateIssue = () => {
 					<li>1 week</li>
 				</ul>
 			</article>
-			<article className="question">
-				<div className="questionContent">
-					<h6 className="questionNumber">Question 1</h6>
-					<h4 className="question">
-						What was your major accomplishment this week?
-					</h4>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-						Integer tempor dolor nec lorem pharetra, quis cursus purus
-						iaculis. Lorem ipsum dolor sit amet, consectetur adipiscing
-						elit.
-					</p>
-				</div>
-				<ul className="stats">
-					<li className="members">
-						<ul className="members">
-							<li>
-								<img
-									alt="avatar"
-									src="https://pbs.twimg.com/profile_images/1175859436398751745/_wHdYjZo_400x400.png"
-								/>
-							</li>
-						</ul>
-						Tony K.
-					</li>
-					<li className="textCount noHover">
-						<em>255</em> / 10000
-					</li>
-				</ul>
-				<textarea
-					required
-					type="text"
-					placeholder="We'll replace this with markdown field"
-				></textarea>
-				<ul className="options">
-					<li>
-						<a className="button big">
-							Wrap up this issue
-						</a>
-					</li>
-				</ul>
-			</article>
+			<CreateIssueQuestion />
 		</section>
-		)
-	};
+	);
+};
