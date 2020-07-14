8f41c4ecc7e6d37b087067e87f827e7579eb071c
Steven Kovar <steven.kovar@gmail.com>
2020-07-14 17:23:55 -0500

    Progress

--- src/components/profiles/UserProfile.jsx
+++ src/components/profiles/UserProfile.jsx
@@ -3,7 +3,29 @@ import React from "react";
 export const UserProfile = () => {
 	return (
 		<>
-			<h3> User Profile goes here</h3>
+			<nav className="login">
+				<ul>
+					<li>
+						<ul className="roundtable logo">
+							<section>
+								<li></li>
+								<li></li>
+								<li></li>
+							</section>
+						</ul>
+						<h3 className="logo">Roundtable</h3>
+					</li>
+				</ul>
+				<div class="get-started">
+					<form>
+						<div className="field">
+						<a className="button" href="javascript:void(0)">
+							Log In
+						</a>
+						</div>
+					</form>
+				</div>
+			</nav>
 		</>
 	);
 };
