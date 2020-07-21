096b18b712e17efe101432429371cf9c1869ece0
Tony <tonyrkovar@gmail.com>
2020-07-21 10:11:35 -0500

    WIP: Trying to figure out how to get the text field to work properly.

--- src/components/textFields/TextField.jsx
+++ src/components/textFields/TextField.jsx
@@ -3,15 +3,49 @@ import React, { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
-export const TextField = ({ modules, formats, inputValue, setInputValue }) => {
+import { CREATE_RESPONSE as createResponses } from "../../resolvers/mutations";
+import { useMutation } from "@apollo/react-hooks";
+
+export const TextField = ({ modules, formats, questionId }) => {
+	const [inputValue, setInputValue] = useState("");
+
+	console.log(questionId);
+
+	const [createResponse] = useMutation(createResponses);
 	// console.log(inputValue);
 	return (
-		<ReactQuill
-			theme="snow"
-			value={inputValue}
-			onChange={setInputValue}
-			modules={modules}
-			formats={formats}
-		/>
+		<>
+			<ReactQuill
+				theme="snow"
+				value={inputValue}
+				onChange={setInputValue}
+				modules={modules}
+				formats={formats}
+			/>
+			<ul className="stats">
+				<li className="textCount noHover">
+					<em>{inputValue.length}</em> / 10000
+				</li>
+			</ul>
+			<article className="question submit">
+				<ul class="options textCenter">
+					<li>
+						<a
+							class="button big"
+							onClick={() =>
+								createResponse({
+									variables: {
+										questionId,
+										content: inputValue,
+									},
+								})
+							}
+						>
+							Submit response
+						</a>
+					</li>
+				</ul>
+			</article>
+		</>
 	);
 };
