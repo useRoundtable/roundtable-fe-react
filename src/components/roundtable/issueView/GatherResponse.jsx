c9107b513db0486e42ea9526ff9b739c6ec1566a
Tony <tonyrkovar@gmail.com>
2020-07-21 15:15:20 -0500

    feat: New component for gathering and submitting user responses

---
+++ src/components/roundtable/issueView/GatherResponse.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { Question } from "../questionResponses/Question";
+import { TextField } from "../../textFields/TextField";
+
+import { useMutation } from "@apollo/react-hooks";
+
+import { CREATE_RESPONSE as sendResponse } from "@resolvers/mutations";
+
+export const GatherResponse = ({ question }) => {
+	const [inputValue, setInputValue] = useState("");
+
+	const [createResponse] = useMutation(sendResponse);
+
+	return (
+		<>
+			<article className="question">
+				<div className="questionContent">
+					<h6 className="questionNumber">Question 3</h6>
+					<h4 className="question">{question.question}</h4>
+					<p>
+						QUESTION CONTEXT WILL GO HERE WHEN IT IS LIVE ON THE DATA BASE
+					</p>
+				</div>
+				<TextField
+					inputValue={inputValue}
+					setInputValue={setInputValue}
+					inputLength={inputValue.length}
+				/>
+				<article className="question submit">
+					<ul class="options textCenter">
+						<li>
+							{console.log(question.id, question.question)}
+							<a
+								class="button big"
+								onClick={() =>
+									createResponse({
+										variables: {
+											question: question.id,
+											content: inputValue,
+										},
+									})
+								}
+							>
+								Submit response
+							</a>
+						</li>
+					</ul>
+				</article>
+			</article>
+		</>
+	);
+};
