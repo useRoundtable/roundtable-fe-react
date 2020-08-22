a0ff6206fd3642bb72ef0fc81b54974c28a935fc
Tony Kovar <tonyrkovar@gmail.com>
2020-08-21 22:15:33 -0500

    feat: added a ton of logic for CreatIssue display logic.

--- src/components/roundtable/createIssue/CreateIssue.jsx
+++ src/components/roundtable/createIssue/CreateIssue.jsx
@@ -5,28 +5,40 @@ import { CreateIssueQuestion } from "./createIssueQuestion/CreateIssueQuestion";
 import { Question } from "../questionResponses/Question";
 import { WeekMap } from "./WeekMap";
 
-import "react-quill/dist/quill.snow.css";
-import ReactQuill from "react-quill";
+import {TextField} from '../../textFields/TextField'
 
-import { useQuery } from "@apollo/react-hooks";
-import { ISSUEBYID as issueById } from "../../../resolvers/queries";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import { CREATE_ISSUE as issueById } from "../../../resolvers/queries";
+import {UPDATE_ISSUE as updateIssue} from '../../../resolvers/mutations'
 
 import {parsePublishDate} from '../../../utils'
 import { getUser } from "../../../auth";
+import { RenderMarkdown } from "../../universalComponents/RenderMarkdown";
 
 export const CreateIssue = () => {
+	let questionNumber = 0
 	const week = [1,2,3,4,5,6,7]
 	const user = getUser()
 	const [publishDate, setPublishDate] = useState(0)
-	const { issueid } = useParams();
+	const { issueid, roundtableId } = useParams();
+	const [promptData, setPromptData] = useState("")
+	
 	const { data, error, loading } = useQuery(issueById, {
 		variables: {
 			id: issueid,
 		},
 	});
+	
+	const [update] = useMutation(updateIssue, {
+		onCompleted({updateIssue}) {
+			window.location.assign(`/roundtable/${roundtableId}/issue/${issueid}`)
+		}
+	})
+	
 	if (loading) {
 		return <h3> loading </h3>;
 	}
+	
 	const time = parsePublishDate(publishDate)
 	const questions = data.issueById.questions;
 	if(user.id !== parseInt(data.issueById.issueAuthor.id)){
@@ -41,7 +53,7 @@ export const CreateIssue = () => {
 			<article class="issueHeader">
 				<h3 class="issue">
 					<span class="number">New Issue</span>
-					<span class="status">Under Construction</span>
+					<span class="status">{data.issueById.currentStatus}</span>
 					<span class="title">
 						{data.issueById.title
 							? `${data.issueById.title}`
@@ -64,19 +76,23 @@ export const CreateIssue = () => {
 							Tony K.
 						</li>
 						<li className="textCount noHover">
-							<em>0</em> / 10000
+						<em>{promptData.length}</em> / 10000
 						</li>
 					</ul>
-					<ReactQuill
-						theme="snow"
-						placeholder="Describe what this issue is about..."
+					{data.issueById.prompt ? 
+						<RenderMarkdown source={data.issueById.prompt}/>
+					:
+					<TextField 
+						inputValue={promptData}
+						setInputValue={setPromptData}
 					/>
+					}
 				</div>
 			</article>
 			<article className="questionList">
 				<ul className="questionList">
 					<h6 className="questionListHeader">
-Collect responses for {data.issueById.title} until {time}.
+						Collect responses for {data.issueById.title} until {time}.
 					</h6>
 					{week.map((day) => {
 						return (
@@ -87,14 +103,32 @@ Collect responses for {data.issueById.title} until {time}.
 				</ul>
 			</article>
 			{questions.map((question) => {
+				questionNumber++
 				return (
 					<Question
 						question={question}
-						questionNum={question.questionNumber}
+						questionNum={questionNumber}
 					/>
 				);
 			})}
 			<CreateIssueQuestion questionNum={questions.length + 1} />
+			<ul>
+				<li>
+					<a 
+					className="button big"
+					onClick={() => {
+						update({
+							variables: {
+								prompt: promptData === "" ? data.issueById.prompt : promptData,
+								title: data.issueById.title,
+								id: issueid,
+								currentStatus: "Collecting Responses!"
+							}
+						})
+					}}
+					>Wrap up this issue</a>
+				</li>
+			</ul>
 		</section>
 	);
 };
