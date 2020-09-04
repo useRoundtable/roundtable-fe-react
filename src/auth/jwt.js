07c10608ca979ad0a43e2be68f4c170754b4940a
Tony <tonyrkovar@gmail.com>
2020-09-03 22:32:16 -0500

    chore: Removing expiration logic from parseToken

--- src/auth/jwt.js
+++ src/auth/jwt.js
@@ -1,10 +1,10 @@
 import jwt from "jsonwebtoken";
 
 export const parseToken = (token) => {
-	const { id, exp } = jwt.decode(token);
-	const now = Math.floor(Date.now() / 1000);
+	const { id } = jwt.decode(token);
+	// const now = Math.floor(Date.now() / 1000);
 
-	if (exp < now) throw new Error("Token is expired");
+	// if (exp < now) throw new Error("Token is expired");
 
 	return { id };
 };
