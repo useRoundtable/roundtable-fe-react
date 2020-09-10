d281c3d8e55b14a07ce78d8b6f451624c495d23f
Tony <tonyrkovar@gmail.com>
2020-09-09 21:27:43 -0500

    chore: I may or may not need this component later.

--- src/components/roundtable/issueView/ResponseInput.jsx
+++ src/components/roundtable/issueView/ResponseInput.jsx
@@ -1,44 +1,9 @@
-import React, { useState } from "react";
-import classNames from "classnames";
+import React from "react";
 
-import { useParams } from "react-router-dom";
-import { useQuery } from "@apollo/client";
-
-import { QuestionCard } from "./questions/QuestionCard";
-import { GatherResponse } from "./GatherResponse";
-
-import { QUESTIONS_BY_ISSUE as QBIID } from "../../../resolvers/queries";
-
-export const ResponseInput = ({ issueStatus }) => {
-	const [activeComponent, setActiveComponent] = useState(0);
-	let qNumber = 0;
-
-	const { issueid } = useParams();
-	const { data, error } = useQuery(QBIID, {
-		variables: {
-			id: issueid,
-		},
-	});
-	if (error) {
-		console.log(error);
-		return <p>Error</p>;
-	}
-	if (issueStatus === "Gathering") {
-		return data.issueById.questions.map((question) => {
-			qNumber++;
-			return (
-				<GatherResponse
-					question={question}
-					qNumber={qNumber}
-					isActive={activeComponent === qNumber ? true : false}
-					setActiveComponent={setActiveComponent}
-				/>
-			);
-		});
-	} else {
-		return data.issueById.questions.map((question) => {
-			qNumber++;
-			return <QuestionCard data={question} qNumber={qNumber} />;
-		});
-	}
+export const ResponseInput = () => {
+	return (
+		<>
+			<p>This is where you will respond.</p>
+		</>
+	);
 };
